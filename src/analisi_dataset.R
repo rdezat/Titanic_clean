dba786f8600eb1f943f222b6cb9ca687457cb388
rdezat <rdezat@uoc.edu>
2019-01-07 20:51:11 +0100

    Revisió

--- src/analisi_dataset.R
+++ src/analisi_dataset.R
@@ -27,12 +27,8 @@ summary(passatgers$Survived)
 passatgers$Survived <- as.factor(passatgers$Survived)
 is.factor(passatgers$Survived)
 
-# Pclass
-summary(passatgers$Pclass)
-# Factoritzem Survived
-passatgers$Pclass <- as.factor(passatgers$Pclass)
-passatgers_test$Pclass <- as.factor(passatgers_test$Pclass)
-is.factor(passatgers$Pclass)
+# Fare
+summary(passatgers$Fare)
 
 # Sex
 summary(passatgers$Sex)
@@ -49,76 +45,121 @@ summary(passatgers$Parch)
 # Mètode KNN per imputar valors perduts
 library(VIM)
 passatgers_2 <- kNN(passatgers)[,1:12]
-passatgers_test <- kNN(passatgers_test)[,1:12]
+passatgers_test <- kNN(passatgers_test)[,1:11]
 summary(passatgers_2$Age)
 
+passatgers[which(passatgers$Fare == 0),1:6]
+passatgers[which(passatgers$Fare == 0),7:12]
+nrow(passatgers[which(passatgers_2$Ticket == 'LINE'),])
+nrow(passatgers[which(passatgers_2$Ticket == 112059),])
+nrow(passatgers[which(passatgers_2$Cabin == 'B94'),])
+nrow(passatgers[which(passatgers_2$Ticket == 239853),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 239854),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 112052),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 239856),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 239855),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 112050),])
+nrow(passatgers_2[which(passatgers_2$Cabin == 'A36'),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 112058),])
+nrow(passatgers_2[which(passatgers_2$Cabin == 'B102'),])
+nrow(passatgers_2[which(passatgers_2$Ticket == 19972),])
+
+mean(passatgers_2[which(passatgers_2$Pclass == 3),'Fare'])
+passatgers_2[which(passatgers_2$Fare == 0 & passatgers_2$Pclass == 3),'Fare'] <- mean(passatgers_2[which(passatgers_2$Pclass == 3),'Fare'])
+mean(passatgers_2[which(passatgers_2$Pclass == 2),'Fare'])
+passatgers_2[which(passatgers_2$Fare == 0 & passatgers_2$Pclass == 2),'Fare'] <- mean(passatgers_2[which(passatgers_2$Pclass == 2),'Fare'])
+mean(passatgers_2[which(passatgers_2$Pclass == 1),'Fare'])
+passatgers_2[which(passatgers_2$Fare == 0 & passatgers_2$Pclass == 1),'Fare'] <- mean(passatgers_2[which(passatgers_2$Pclass == 1),'Fare'])
+
 # Boxplots Age, SibSp i Parch
-variables <- c("Age", "SibSp", "Parch")
+variables <- c("Age", "Fare")
 for (name in variables){
   boxplot(passatgers_2[,name], main=name)
 }
 # Valors extrems
 boxplot.stats(passatgers_2$Age)$out
-boxplot.stats(passatgers_2$SibSp)$out
-boxplot.stats(passatgers_2$Parch)$out
+boxplot.stats(passatgers_2$Fare)$out
+
+head(passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out)),1:6])
+head(passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out)),7:12])
+
+passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out) & passatgers$Pclass == 3),1:6]
+passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out) & passatgers$Pclass == 3),7:12]
+
+passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out) & passatgers$Pclass == 2),1:6]
+passatgers[which(passatgers$Fare >= min(boxplot.stats(passatgers_2$Fare)$out) & passatgers$Pclass == 2),7:12]
+
+head(passatgers)
+head(passatgers_test)
+passatgers_test$Survived <- NaN
+passatgers_test <- subset (passatgers_test, select=c(1,12,2,3,4,5,6,7,8,9,10,11))
+passatgers_all <- rbind(passatgers, passatgers_test)
+nrow(passatgers_all)
+library(dplyr)
+grupos_sibsp <- group_by(passatgers_all, passatgers_all$SibSp)
+resum_grupos_sibsp <- summarise(grupos_sibsp, num = n())
+resum_grupos_sibsp
 
 # Passatgers amb 8 germans i cònjugues
-passatgers[which(passatgers$SibSp == 8),1:6]
-passatgers[which(passatgers$SibSp == 8),7:12]
-# Passatgers amb 8 germans i cònjugues (test)
-passatgers_test[which(passatgers_test$SibSp == 8),1:6]
-passatgers_test[which(passatgers_test$SibSp == 8),7:11]
+passatgers_all[which(passatgers_all$SibSp == 8),1:6]
+passatgers_all[which(passatgers_all$SibSp == 8),7:12]
+
 
 # Passatgers amb 5 germans i cònjugues
-passatgers[which(passatgers$SibSp == 5),1:6]
-passatgers[which(passatgers$SibSp == 5),7:12]
-# Passatgers amb 5 germans i cònjugues (test)
-passatgers_test[which(passatgers_test$SibSp == 5),1:6]
-passatgers_test[which(passatgers_test$SibSp == 5),7:11]
+passatgers_all[which(passatgers_all$SibSp == 5),1:6]
+passatgers_all[which(passatgers_all$SibSp == 5),7:12]
 
-# Valors extrems sense 0
-boxplot.stats(passatgers_2$Parch[which(passatgers_2$Parch != 0)])$out
+
+# Valors extrems Parch
+grupos_parch <- group_by(passatgers_all, passatgers_all$Parch)
+resum_grupos_parch <- summarise(grupos_parch, num = n())
+resum_grupos_parch
+
+# Passatgers amb 9 pares o fills
+passatgers_all[which(passatgers_all$Parch == 9),1:6]
+passatgers_all[which(passatgers_all$Parch == 9),7:12]
 
 # Passatgers amb 6 pares o fills
-passatgers[which(passatgers$Parch == 6),1:6]
-passatgers[which(passatgers$Parch == 6),7:12]
-# Passatgers amb 6 pares o fills (test)
-passatgers_test[which(passatgers_test$Parch == 6),1:6]
-passatgers_test[which(passatgers_test$Parch == 6),7:12]
+passatgers_all[which(passatgers_all$Parch == 6),1:6]
+passatgers_all[which(passatgers_all$Parch == 6),7:12]
 
 #Assignar lletres als factors de Pclass i Sex
 library(car)
-passatgers_2$Pclass <- recode(passatgers_2$Pclass,"1='H';2='M';3='L'")
-summary(passatgers_2$Pclass)
 passatgers_2$Sex <- recode(passatgers_2$Sex,"'male'='M';'female'='F'")
 summary(passatgers_2$Sex)
 
-# Reordenem la variable Pclass en la nova variable PclassR
-passatgers_2$PclassR <- relevel(passatgers_2$Pclass, ref = "H")
-head(passatgers_2$PclassR)
 # Reordenem la variable Sex en la nova variable SexR
 passatgers_2$SexR <- relevel(passatgers_2$Sex, ref = "F")
 head(passatgers_2$SexR)
 
+library(nortest)
+plot(density(passatgers_2$Age))
+pearson.test(passatgers_2$Age)
+plot(density(passatgers_2$Fare))
+pearson.test(passatgers_2$Fare)
+
+bartlett.test(passatgers_2$Age~passatgers_2$Survived, data=passatgers_2)
+bartlett.test(passatgers_2$Fare~passatgers_2$Survived, data=passatgers_2)
+
+
+cor(passatgers_2$Fare, passatgers_2$Pclass)
+
+# Escriptura del fitxer netejat
+passatgers_clean <- cbind(passatgers_2[,1:4],passatgers_2$SexR,passatgers_2[,6:12])
+write.csv(passatgers_clean, file = paste(ruta, "Titanic_clean.csv", sep = ""), sep = ",")
+
 # Generem el model
-model_logit <- glm(passatgers_2$Survived ~ passatgers_2$PclassR 
+model_logit <- glm(passatgers_2$Survived ~ passatgers_2$Fare 
                    + passatgers_2$SexR + passatgers_2$Age + passatgers_2$SibSp
                    + passatgers_2$Parch, family = "binomial")
 summary(model_logit)
 
-# Generem el model sense Parch
-model_logit_2 <- glm(passatgers_2$Survived ~ passatgers_2$PclassR 
-                     + passatgers_2$SexR + passatgers_2$Age + passatgers_2$SibSp
-                     , family = "binomial")
-summary(model_logit_2)
-
-
 #coefficients
-coeff_beta <- coefficients(model_logit_2)
+coeff_beta <- coefficients(model_logit)
 coeff_beta
 
 # Calculem els valors predits
-valors_predits <- predict(model_logit_2,passatgers_2,type = "response")
+valors_predits <- predict(model_logit,passatgers_2,type = "response")
 head(valors_predits)
 # Interpretem els resultats amb el llindar indicat
 clase_predita <- ifelse(valors_predits>0.7, 1,0)
@@ -157,11 +198,12 @@ valor_pre_neg <- tn/negatius_predit
 kable(data.frame(Mesura=c("Exactitud", "Ratio d'error", "Sensibilitat", "Especificitat", "Precisió", "Valor de predicció de negatius"),
                  Valor=c(exactitut, ratio_error, sensibilitat, especificitat, precisio, valor_pre_neg)), align = c("l","l","l","l","l","l"))
 
-stripchart(valors_predits~passatgers_2$PclassR,pch=19)
+stripchart(valors_predits~passatgers_2$Fare,pch=19)
 
 passatgers_2$Ppre <- valors_predits
 passatgers_2$Pre <- data$pre
-p_low_sur_pre <- passatgers_2[which(passatgers_2$Ppre >= 0.7 & passatgers_2$PclassR == 'L'),]
+passatgers_2[which(passatgers_2$Ppre >= 0.7),]
+p_low_sur_pre <- passatgers_2[which(passatgers_2$Ppre >= 0.7 & passatgers_2$Fare <= quantile(passatgers_2$Fare)[2]),]
 p_low_sur_pre[4:7]
 head(p_low_sur_pre[4:7])
 
